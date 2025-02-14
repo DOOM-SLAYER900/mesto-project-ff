87405d0fc10dc8fe659ef275e78913bc6dbde08f
DOOM-SLAYER900 <procombat30@gmail.com>
2025-02-15 00:06:10 +0700

    SPRINT-FIVE-FIRST-FIX

--- scripts/index.js
+++ scripts/index.js
@@ -11,21 +11,24 @@ const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
 
 function createCard(cardElement) {
-  const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
-  const cardImageSrc = cardItem.querySelector(".card__image").src = cardElement.link;
-  const cardImageAlt = cardItem.querySelector(".card__image").alt = cardElement.name;
-  const cardTitle = cardItem.querySelector(".card__title").textContent = cardElement.name;
+  const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImageSrc = (cardItem.querySelector(".card__image").src =
+    cardElement.link);
+  const cardImageAlt = (cardItem.querySelector(".card__image").alt =
+    cardElement.name);
+  const cardTitle = (cardItem.querySelector(".card__title").textContent =
+    cardElement.name);
   const deleteButton = cardItem.querySelector(".card__delete-button");
   deleteButton.addEventListener("click", deleteCard);
   return cardItem;
-};
+}
 
 function deleteCard() {
   const deleteButton = cardList.querySelector(".card__delete-button");
-    const cardForRemove = deleteButton.closest('.card');
-    cardForRemove.remove();
-};
+  const cardForRemove = deleteButton.closest(".card");
+  cardForRemove.remove();
+}
 
 initialCards.forEach(function addCard(cardValue) {
-  cardList.append(createCard(cardValue))});
-
+  cardList.append(createCard(cardValue));
+});
