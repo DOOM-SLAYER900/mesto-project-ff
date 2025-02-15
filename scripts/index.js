c00c7ebc33e5830f6d44bb08699aa8307d1214a3
DOOM-SLAYER900 <procombat30@gmail.com>
2025-02-15 17:44:34 +0700

    SPRINT-FIVE-SECOND-FIX

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@
 const cardTemplate = document.querySelector("#card-template").content;
 const cardList = document.querySelector(".places__list");
 
-function createCard(cardElement) {
+function createCard(cardElement, func) {
   const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImageSrc = (cardItem.querySelector(".card__image").src =
     cardElement.link);
@@ -18,17 +18,16 @@ function createCard(cardElement) {
     cardElement.name);
   const cardTitle = (cardItem.querySelector(".card__title").textContent =
     cardElement.name);
-  const deleteButton = cardItem.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  const deleteButton = cardItem
+    .querySelector(".card__delete-button")
+    .addEventListener("click", () => func(cardItem));
   return cardItem;
 }
 
-function deleteCard() {
-  const deleteButton = cardList.querySelector(".card__delete-button");
-  const cardForRemove = deleteButton.closest(".card");
+const deleteFunc = function deleteCard(cardForRemove) {
   cardForRemove.remove();
-}
+};
 
 initialCards.forEach(function addCard(cardValue) {
-  cardList.append(createCard(cardValue));
+  cardList.append(createCard(cardValue, deleteFunc));
 });
